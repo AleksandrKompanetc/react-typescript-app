24405278992750f46e4b00d9c421e3f0aaa5b15d
AleksandrKompanetc <moneymacker7@gmail.com>
2024-12-23 22:01:48 +0200

    added users

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import React, {useState} from 'react';
 import './App.css';
 
+type User = {
+  id: number;
+  name: string;
+  email: string;
+}
+
 function App() {
+  const [users, setUsers] = useState<User[]>([
+    {id: 1, name: 'Alice', email: 'alice@example.com'},
+    {id: 2, name: 'Bob', email: 'bob@example.com'}
+  ])
   return (
     <div className="App">
-      
+      Users
     </div>
   );
 }
